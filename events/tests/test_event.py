023499b21bf0878884a7ca8eab86108592ce594f
Jonathan Scott <jonathan@jscott.me>
2014-11-25 20:42:03 +0000

    Don't commit un-needed events

--- events/tests/test_event.py
+++ events/tests/test_event.py
@@ -12,9 +12,9 @@ class TestIndex(TestCase):
 
     def test_is_future(self):
         """ Test that is_future works. """
-        past_event = mommy.make("Event", datetime=datetime.now(pytz.utc) -
-                                timedelta(days=20))
+        past_event = mommy.prepare("Event", datetime=datetime.now(pytz.utc) -
+                                   timedelta(days=20))
         self.assertFalse(past_event.is_future())
-        future_event = mommy.make("Event", datetime=datetime.now(pytz.utc) +
-                                  timedelta(days=20))
+        future_event = mommy.prepare("Event", datetime=datetime.now(pytz.utc) +
+                                     timedelta(days=20))
         self.assertTrue(future_event.is_future())
