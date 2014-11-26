51b123bb76bc2c13b45d834c8b827cb4d9fec20c
Jonathan Scott <jonathan@jscott.me>
2014-11-26 15:15:10 +0000

    match standards

--- events/tests/test_event_manager.py
+++ events/tests/test_event_manager.py
@@ -15,16 +15,16 @@ class TestEventManager(TestCase):
         """ Test that latest_event finds the next future event. """
         next_event = mommy.make("Event", datetime=datetime.now(pytz.utc) +
                                 timedelta(days=5))
-        future_event = mommy.make("Event", datetime=datetime.now(pytz.utc) +
-                                  timedelta(days=20))
-        previous_event = mommy.make("Event", datetime=datetime.now(pytz.utc) -
-                                    timedelta(days=5))
+        mommy.make("Event", datetime=datetime.now(pytz.utc) +
+                   timedelta(days=20))
+        mommy.make("Event", datetime=datetime.now(pytz.utc) -
+                   timedelta(days=5))
         self.assertEqual(next_event, Event.objects.latest_event())
 
     def test_latest_event_finds_previous_event(self):
         """ Test that latest_event finds the next future event. """
         last_event = mommy.make("Event", datetime=datetime.now(pytz.utc) -
                                 timedelta(days=5))
-        previous_event = mommy.make("Event", datetime=datetime.now(pytz.utc) -
-                                    timedelta(days=20))
+        mommy.make("Event", datetime=datetime.now(pytz.utc) -
+                   timedelta(days=20))
         self.assertEqual(last_event, Event.objects.latest_event())
