4618e499c1c6a0fdbb62ea2351f5bdb8748197d1
Jonathan Scott <jonathan@jscott.me>
2015-01-08 19:21:30 +0000

    Tickets typo

--- events/templatetags/tickets.py
+++ events/templatetags/tickets.py
@@ -8,4 +8,4 @@ register = template.Library()
 @register.filter(name='has_tickets')
 def user_has_tickets(user, event):
     """ Return True if the user has tickets for this event. """
-    return event.votes.filter(user=user).count() > 0
+    return event.tickets.filter(user=user).count() > 0
