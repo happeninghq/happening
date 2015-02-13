ec6122f8f4431b56cb3644c5a9c53a9b304c2a3c
Jonathan Scott <jonathan@jscott.me>
2015-02-13 15:30:46 +0000

    Small adjustments to stripe

--- members/forms.py
+++ members/forms.py
@@ -75,11 +75,18 @@ class PaymentForm(forms.Form):
                                ))
     other_amount = forms.IntegerField(required=False)
 
+    def clean_other_amount(self):
+        u""" Ensure other amount is at least £5. """
+        if self.cleaned_data['amount'] == 'other':
+            if self.cleaned_data['other_amount'] < 5:
+                raise forms.ValidationError("You must pay at least £5")
+        return self.cleaned_data['other_amount']
+
     @property
     def selected_amount(self):
         """ Get the actual amount the user wants to pay. """
         if self.cleaned_data['amount'] == 'other':
-            return self.other_amount
+            return self.cleaned_data['other_amount']
         else:
             return int(self.cleaned_data['amount'])
 
