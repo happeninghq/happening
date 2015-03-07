53626aaffa171a84a2c09a7f82500dca827a33ad
Jonathan Scott <jonathan@jscott.me>
2015-03-07 21:12:48 +0000

    Time page rendering in debug

--- website/settings.py
+++ website/settings.py
@@ -63,6 +63,7 @@ INSTALLED_APPS = (
     'foundationform',
 
     'debug_toolbar',
+    'template_profiler_panel',
 
     'website',
     'external',
@@ -253,7 +254,25 @@ STRIPE_SECRET_KEY = os.environ.get("STRIPE_SECRET_KEY",
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 # Configure Django Debug Toolbar
-JQUERY_URL = ""
+DEBUG_TOOLBAR_PANELS = [
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+    'template_profiler_panel.panels.template.TemplateProfilerPanel'
+]
+
+DEBUG_TOOLBAR_CONFIG = {
+    "JQUERY_URL": ""
+}
 
 # ---------------------------
 # EDIT THINGS BELOW THIS LINE
