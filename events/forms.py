b428d51361b5468e038b3bd484d254af3b14868d
Jonathan Scott <jonathan@jscott.me>
2015-04-29 20:06:06 +0100

    fix small bug with 1 ticket restriction

--- events/forms.py
+++ events/forms.py
@@ -11,14 +11,15 @@ class TicketForm(forms.Form):
 
     def __init__(self, *args, **kwargs):
         """Initialise the PurchaseForm with an event."""
-        event = kwargs.pop("event")
-        max_tickets = event.remaining_tickets + 1
+        kwargs.pop("event")
+        # max_tickets = event.remaining_tickets + 1
         if 'max_tickets' in kwargs:
-            max_tickets = kwargs.pop("max_tickets")
+            kwargs.pop("max_tickets")
+        #     max_tickets = kwargs.pop("max_tickets")
         super(TicketForm, self).__init__(*args, **kwargs)
 
-        choices = [
-            (str(x), str(x)) for x in range(1, max_tickets)]
+        # choices = [
+        #     (str(x), str(x)) for x in range(1, max_tickets)]
         # self.fields['quantity'].choices = choices
 
     quantity = forms.ChoiceField(label='Quantity', choices=(("1", "1"),))
