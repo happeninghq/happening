4e7cd3fe75759c9c6e37c2cb14cbf681bfc7a8d8
Jonathan Scott <jonathan@jscott.me>
2015-05-09 10:19:31 +0100

    Fix multiple primary key bug

--- events/migrations/0023_eventpreset.py
+++ events/migrations/0023_eventpreset.py
@@ -14,7 +14,8 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='EventPreset',
             fields=[
-                ('name', models.CharField(max_length=255, serialize=False, primary_key=True)),
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=255)),
                 ('value', models.TextField()),
             ],
             options={
