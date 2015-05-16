d892914381a3067fdd04d6d0af0aceda0c092039
Jonathan Scott <jonathan@jscott.me>
2015-05-16 12:02:25 +0100

    test staff

---
+++ staff/tests/test_staff.py
@@ -0,0 +1,31 @@
+"""Test sending emails."""
+
+from happening.tests import TestCase
+from model_mommy import mommy
+from django.conf import settings
+
+
+class TestStaff(TestCase):
+
+    """Test staff views."""
+
+    def setUp(self):
+        """Set up users."""
+        self.user = mommy.make(settings.AUTH_USER_MODEL, is_staff=True)
+        self.user.set_password("password")
+        self.user.save()
+
+        self.non_staff_user = mommy.make(settings.AUTH_USER_MODEL)
+        self.non_staff_user.set_password("password")
+        self.non_staff_user.save()
+
+    def test_dashboard(self):
+        """Test dashboard loads only for staff."""
+        self.client.login(username=self.user.username, password="password")
+        response = self.client.get("/staff/")
+        self.assertEquals(response.status_code, 200)
+
+        self.client.login(username=self.non_staff_user.username,
+                          password="password")
+        response = self.client.get("/staff/")
+        self.assertEquals(response.status_code, 302)
