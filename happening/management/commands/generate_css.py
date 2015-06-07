d3a0ce1dec99d19cae39535fdb6e331547515867
Jonathan Scott <jonathan@jscott.me>
2015-06-07 19:58:47 +0100

    also create directory

--- happening/management/commands/generate_css.py
+++ happening/management/commands/generate_css.py
@@ -1,6 +1,7 @@
 """Generate CSS according to settings."""
 from django.core.management.base import BaseCommand
 from happening.appearance import generate_css
+import os
 
 
 class Command(BaseCommand):
@@ -11,5 +12,7 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         """Generate CSS according to settings."""
+        if not os.path.exists("static/css"):
+            os.makedirs("static/css")
         with open("static/css/generated.css", "w+") as o:
             o.write(generate_css())
