76a633452abc14cc72a0ec12034e74720fb88603
Jonathan Scott <jonathan@jscott.me>
2015-06-08 10:49:47 +0100

    correctly delete old media

--- happening/periodictasks.py
+++ happening/periodictasks.py
@@ -1,17 +1,16 @@
 """General period tasks."""
 from periodically.decorators import daily
-import time
-import os
-from django.conf import settings
+from django.core.files.storage import default_storage
+from datetime import datetime, timedelta
 
 
 @daily
 def delete_old_temporary_media():
     """Delete temporary media older than 24 hours."""
-    now = time.time()
-    path = '%s/tmp' % settings.MEDIA_ROOT
-    for f in os.listdir(path):
-        if os.stat(os.path.join(path, f)).st_mtime < now - 86400:
+    one_day_old = datetime.now() - timedelta(days=1)
+    for f in default_storage.listdir("tmp"):
+        modified_time = default_storage.modified_time("tmp/%s" % f)
+        if modified_time < one_day_old:
             # At least 24 hours old
             print "Deleting", f
-            os.remove(os.path.join(path, f))
+            default_storage.delete("tmp/%s" % f)
