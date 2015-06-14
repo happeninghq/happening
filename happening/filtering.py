4adc032c2511244a0ebdbf54a916b694bd3c0ad2
Jonathan Scott <jonathan@jscott.me>
2015-06-14 16:36:10 +0100

    Add basic public query language

---
+++ happening/filtering.py
@@ -0,0 +1,54 @@
+"""Filtering."""
+from django.contrib.auth.models import User
+from django.db.models import Count
+
+
+def split_attributes(str):
+    """Split a HQL query into attributes."""
+    attributes = []
+    num_parenthesis = 0
+    current_attribute = ""
+    for s in str:
+        if s == " " and num_parenthesis == 0:
+            if current_attribute:
+                attributes.append(current_attribute)
+            current_attribute = ""
+        else:
+            current_attribute += s
+            if s == "(":
+                num_parenthesis += 1
+            elif s == ")":
+                num_parenthesis -= 1
+    if current_attribute:
+        attributes.append(current_attribute)
+
+
+def query(str, data_type=User):
+    """Get the results of a HQL Query."""
+    attributes = split_attributes(str)
+
+    q = data_type.objects.all()
+
+    for attribute in attributes:
+        key, value = attribute.split(":", 1)
+
+        key_parts = key.split("__")
+        if len(key_parts) > 1:
+            if key_parts[1] == 'count':
+                q = q.annotate(Count(key_parts[0]))
+            if key_parts[1] == 'has':
+                # We now pull out everything in the brackets
+                # and evaluate that
+
+                # This only works if the "has" is on the "many" side of the
+                # relationship. TODO: If needed, as one for the "one" side.
+                related_model = data_type._meta.get_field_by_name(
+                    key_parts[0])[0].field.model
+                results = query(value[1:-1], related_model)
+
+                q = q.filter(**{key_parts[0] + "__in": results})
+                continue
+
+        q = q.filter(**{key: value})
+
+    return q
---
+++ happening/tests/test_filtering.py
@@ -0,0 +1,76 @@
+"""Test filtering."""
+
+from happening.tests import TestCase
+from model_mommy import mommy
+from django.conf import settings
+from happening import filtering
+
+
+class TestFiltering(TestCase):
+
+    """Test filtering."""
+
+    def test_simple_filter(self):
+        """Test simply filter all Members."""
+        # Create 5 users
+        for i in range(1, 6):
+            mommy.make(settings.AUTH_USER_MODEL, id=i)
+
+        results = filtering.query("")
+        self.assertEqual(len(results), 5)
+
+    def test_filter_by_id(self):
+        """Test filter by id."""
+        # Create 5 users
+        for i in range(1, 6):
+            mommy.make(settings.AUTH_USER_MODEL, id=i)
+
+        results = filtering.query("id:1")
+        self.assertEqual(results[0].id, 1)
+
+        results = filtering.query("id:3")
+        self.assertEqual(results[0].id, 3)
+
+    def test_filter_using_gt(self):
+        """Test using django __gt filter (if this works, others will)."""
+        # Create 5 users
+        for i in range(1, 6):
+            mommy.make(settings.AUTH_USER_MODEL, id=i)
+
+        results = filtering.query("")
+        self.assertEqual(len(results), 5)
+
+        results = filtering.query("id__gt:1")
+        self.assertEqual(len(results), 4)
+
+    def test_filter_related(self):
+        """Test getting members who have tickets for a given event."""
+        # Create 5 users
+        user1 = mommy.make(settings.AUTH_USER_MODEL)
+        user2 = mommy.make(settings.AUTH_USER_MODEL)
+        user3 = mommy.make(settings.AUTH_USER_MODEL)
+        mommy.make(settings.AUTH_USER_MODEL)
+
+        event1 = mommy.make("Event")
+        event2 = mommy.make("Event")
+        mommy.make("Ticket", event=event1, user=user1)
+        mommy.make("Ticket", event=event1, user=user3, cancelled=True)
+        mommy.make("Ticket", event=event2, user=user2)
+        mommy.make("Ticket", event=event2, user=user1, cancelled=True)
+
+        # Members who have tickets
+        results = filtering.query("tickets__count__gt:0")
+        self.assertEqual(len(results), 3)
+
+        # Members who have tickets to event 1
+        results = filtering.query("tickets__event__id:1")
+        self.assertEqual(len(results), 2)
+
+        # Members who have cancelled tickets for event 1
+        results = filtering.query("tickets__has:(event__id:1 cancelled:True)")
+        self.assertEqual(len(results), 1)
+
+        # Members who have tickets for event 1 and 2
+        results = filtering.query("tickets__has:(event__id:1) " +
+                                  "tickets__has:(event__id:2)")
+        self.assertEqual(len(results), 1)
