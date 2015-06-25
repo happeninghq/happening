24fd2df573af4a28329d3e04e5414c12ac7b7c36
Jonathan Scott <jonathan@jscott.me>
2015-06-25 11:44:49 +0100

    Escape single quotes in email widget

--- happening/templatetags/strings.py
+++ happening/templatetags/strings.py
@@ -10,3 +10,21 @@ register = template.Library()
 def spaces(text):
     """Convert CamelCase into Space Separated."""
     return convert_to_spaces(text)
+
+
+# Just a substring unlikely to occur so can be used as placeholder
+UNUSED_SUBSTRING = "8734oflwehiu2873r87yor3f"
+
+
+@register.filter()
+def search(value, search):
+    """Replace search with the unused substring."""
+    return value.replace(search, UNUSED_SUBSTRING)
+    # return re.sub(search, '#f4x@SgXXmS', value)
+
+
+@register.filter()
+def replace(value, replace):
+    """Replace the unused substring with replace."""
+    return value.replace(UNUSED_SUBSTRING, replace)
+    # return re.sub('#f4x@SgXXmS', replace, value)
