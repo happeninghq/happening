4751fde95cbadda7e47d2434ca06b3bb29f5f27d
Jonathan Scott <jonathan@jscott.me>
2015-08-24 23:00:20 +0100

    fix notification dropdown

--- static/js/blocks/notifications-list.js
+++ static/js/blocks/notifications-list.js
@@ -4,6 +4,11 @@ $(function() {
     $('.notifications-list-link').click(function() {
         $.post("/notifications/mark-read");
         $('.notification-button__unread').removeClass('notification-button__unread--unread');
+
+        if ($(this).hasClass('notification-button__unread')) {
+            $('.notifications-list-link:first').click();
+            return false;
+        }
     });
 
     // Make links work on notifications
