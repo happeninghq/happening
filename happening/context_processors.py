939d69eb3195181538dbe321ddbb672a8816730c
Jonathan Scott <jonathan@jscott.me>
2015-09-02 17:57:03 +0100

    Fix logo on login page

--- happening/context_processors.py
+++ happening/context_processors.py
@@ -4,4 +4,4 @@ from django.contrib.sites.models import Site
 
 def site(request):
     """Return happening site."""
-    return {"site": Site.objects.first().happening_site}
+    return {"happening_site": Site.objects.first().happening_site}
