dce4c86d3bb30b474e28d21fcef9ece304556e23
Jonathan Scott <jonathan@jscott.me>
2016-06-11 12:34:08 +0100

    We don't need node_modules in static files now

--- src/happening/settings.py
+++ src/happening/settings.py
@@ -184,8 +184,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
-    os.path.join(BASE_DIR, 'node_modules'),
+    os.path.join(BASE_DIR, 'static')
 ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'compiled_static')
