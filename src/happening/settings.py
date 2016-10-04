888c39a06b145cc2263f9f024bf69f4207d80ee1
Jonathan Scott <jonathan@jscott.me>
2016-10-04 12:03:30 +0100

    disable SSL during test

--- src/happening/settings.py
+++ src/happening/settings.py
@@ -28,7 +28,8 @@ DEBUG = bool(os.environ.get('DJANGO_DEBUG', 'False') == 'True') or \
 USE_DEBUG_TOOLBAR = DEBUG and (bool(os.environ.get('USE_DEBUG_TOOLBAR',
                                     'False') == 'True'))
 USE_LIVE_DATA = bool(os.environ.get('USE_LIVE_DATA', str(not DEBUG)) == 'True')
-DISABLE_SSL = bool(os.environ.get('DISABLE_SSL', 'False') == 'True')
+DISABLE_SSL = bool(os.environ.get('DISABLE_SSL', 'False') == 'True') or \
+    'HAPPENING_TESTING' in os.environ
 
 ALLOWED_HOSTS = ['*']
 
