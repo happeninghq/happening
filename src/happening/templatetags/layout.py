363db73ac7ffd75e4d7de6dd29a89bfebf7a4734
Jonathan Scott <jonathan@jscott.me>
2016-11-20 23:14:37 +0000

    Allow url parameters on navigation

---
+++ src/happening/templatetags/layout.py
@@ -0,0 +1,35 @@
+"""Template tags relating to layout."""
+
+from django import template
+from django.template.loader import render_to_string
+from django.urls import reverse
+
+register = template.Library()
+
+
+@register.simple_tag(takes_context=True)
+def primary_navigation_item(context, *params, **kwargs):
+    """Render navigation items."""
+    context["text"] = kwargs.pop("text")
+    context["link_name"] = kwargs.pop("link")
+    context["icon"] = kwargs.pop("icon")
+    context["link"] = reverse(context["link_name"], kwargs=kwargs)
+    return render_to_string("_primary_navigation_item.html", context)
+
+
+@register.simple_tag(takes_context=True)
+def secondary_navigation_item(context, *params, **kwargs):
+    """Render navigation items."""
+    context["text"] = kwargs.pop("text")
+    context["link_name"] = kwargs.pop("link")
+    context["link"] = reverse(context["link_name"], kwargs=kwargs)
+    return render_to_string("_secondary_navigation_item.html", context)
+
+
+@register.simple_tag(takes_context=True)
+def tertiary_navigation_item(context, *params, **kwargs):
+    """Render navigation items."""
+    context["text"] = kwargs.pop("text")
+    context["link_name"] = kwargs.pop("link")
+    context["link"] = reverse(context["link_name"], kwargs=kwargs)
+    return render_to_string("_tertiary_navigation_item.html", context)
