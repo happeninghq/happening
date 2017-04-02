e57b3f0cb6ea44100ea56aee6780bdf211909c1d
Jonathan Scott <jonathan@jscott.me>
2017-04-02 11:37:54 +0100

    have web server wait for postgres to start up

--- src/docker_web_entry.py
+++ src/docker_web_entry.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import psycopg2
+import time
 from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
 DEFAULT_DATABASE_URL = "postgres://postgres:@db:5432/happening"
@@ -15,28 +16,40 @@ if not DATABASE_URL:
     sys.exit(1)
 print("DATABASE_URL", DATABASE_URL)
 
-try:
-    conn = psycopg2.connect(DATABASE_URL)
-    conn.close()
-except psycopg2.OperationalError as e:
-    if ('database "happening" does not exist' in str(e) and
-            DATABASE_URL == DEFAULT_DATABASE_URL):
-        # We're using the default database and the database hasn't been created
-        print("The database doesn't exist. Creating it.")
-        conn = psycopg2.connect("postgres://postgres:@db:5432/")
-        conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-        cur = conn.cursor()
-        cur.execute('CREATE DATABASE happening')
-        cur.close()
-        conn.close()
 
-        os.system("./manage.py migrate")
-        os.system("./manage.py loaddata fixtures/common fixtures/users")
-    else:
+def get_database_connection(url, tries=3):
+    try:
+        conn = psycopg2.connect(url)
+        conn.close()
+    except psycopg2.OperationalError as e:
+        if ('could not connect to server' in str(e)):
+            if tries <= 0:
+                raise e
+            else:
+                print("Waiting for database...")
+                time.sleep(5)
+                get_database_connection(url, tries - 1)
+        elif ('database "happening" does not exist' in str(e) and
+                url == DEFAULT_DATABASE_URL):
+            # We're using the default database and the database hasn't been created
+            print("The database doesn't exist. Creating it.")
+            conn = psycopg2.connect("postgres://postgres:@db:5432/")
+            conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+            cur = conn.cursor()
+            cur.execute('CREATE DATABASE happening')
+            cur.close()
+            conn.close()
+
+            os.system("./manage.py migrate")
+            os.system("./manage.py loaddata fixtures/common fixtures/users")
+        else:
+            raise e
+    except Exception as e:
+        print("There was a database error.")
         raise e
-except Exception as e:
-    print("There was a database error.")
-    raise e
+
+
+get_database_connection(DATABASE_URL)
 
 # Running required migrations
 os.system("./manage.py migrate")
